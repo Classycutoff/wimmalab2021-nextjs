0b2f8f11af90bc6b782f7d0c8d8135e48e740c5c
M2110 <m2110@student.jamk.fi>
2021-07-03 21:30:11 +0300

    Add content

--- pages/students.js
+++ pages/students.js
@@ -24,6 +24,40 @@ const Students = () => {
           <p>{t('h1-subheading')}</p>
         </div>
       </section>
+      <section className="content__section">
+        <div>
+          <h2>{t('h2-students')}</h2>
+          <div className="simple-grid">
+            <div>
+              <h3>{t('h3-wimma-lab-offers')}</h3>
+              <p>{t('p-wimma-lab-offers-1')}</p>
+              <p>{t('p-wimma-lab-offers-2')}</p>
+            </div>
+            <div></div>
+            <div></div>
+            <div>
+              <h3>{t('h3-apply-to-wimma-lab')}</h3>
+              <p>{t('p-apply-to-wimma-lab-1')}</p>
+              <p>{t('p-apply-to-wimma-lab-2')}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="content__section">
+        <div>
+          <h2>{t('h2-responsibility')}</h2>
+          <div className="simple-grid">
+            <div>
+              <h3>{t('h3-team-leader')}</h3>
+              <p>{t('p-team-leader')}</p>
+            </div>
+            <div>
+              <h3>{t('h3-student-coach')}</h3>
+              <p>{t('p-student-coach')}</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 };
