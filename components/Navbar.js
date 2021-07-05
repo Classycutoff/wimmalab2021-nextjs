33b40f2c26e903b2a95ae73a9d33909b10696218
M2110 <m2110@student.jamk.fi>
2021-07-05 12:58:33 +0300

    Improve styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,31 +9,33 @@ export function Navbar({ t }) {
 
   return (
     <div className="navbar">
-      <Link href="/">
-        <a>
-          <Image src={logoPicture} alt="WIMMA Lab" width="46" height="24" />
-        </a>
-      </Link>
-      <ul>
-        <li>
-          <Link href="/students">
-            <a>{t('students')}</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/companies">
-            <a>{t('companies')}</a>
-          </Link>
-        </li>
-        <li>
-          <Link href={router.route} locale="fi">
-            <a>FI</a>
-          </Link>
-          <Link href={router.route} locale="en">
-            <a>EN</a>
-          </Link>
-        </li>
-      </ul>
+      <div className="navbar__container">
+        <Link href="/">
+          <a>
+            <Image src={logoPicture} alt="WIMMA Lab" width="40" height="20" />
+          </a>
+        </Link>
+        <ul>
+          <li>
+            <Link href="/students">
+              <a>{t('students')}</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/companies">
+              <a>{t('companies')}</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={router.route} locale="fi">
+              <a>FI</a>
+            </Link>
+            <Link href={router.route} locale="en">
+              <a>EN</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
