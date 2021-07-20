25eacc260d5832e51a4f9a0ca9931ed04c33f813
M3920 <m3920@student.jamk.fi>
2021-07-20 12:35:26 +0300

    new images and styles added to company page

--- pages/companies.js
+++ pages/companies.js
@@ -6,7 +6,7 @@ import Layout, { siteTitle } from '/components/Layout';
 
 import ImageWrapper from '../components/ImageWrapper';
 // import companyBg from '../public/assets/company.jpg';
-import Img2 from '../public/assets/company2.png';
+import Img2 from '../public/assets/software-development-image.png';
 
 const Companies = () => {
   const { t } = useTranslation('companies');
@@ -19,9 +19,10 @@ const Companies = () => {
         </title>
       </Head>
 
-      {/* Information for companies */}
+      {/* information for companies */}
       <section className="content__section hero--companies">
         <div className="hero__container">
+          {/* <Image src={companyBg} layout="fill" objectFit="cover" objectPosition="center" /> */}
           <p> {t('p-info')} </p>
           <h1 className="hero__h1">{t('h1')}</h1>
           <p className="hero__p">{t('h1-subheading')}</p>
@@ -54,20 +55,20 @@ const Companies = () => {
             <h2>{t('h2-how-to-participate')}</h2>
             <p>{t('p-how-to-participate')}</p>
           </div>
-          <div className="simple-grid">
-            <div>
+          <div className="simple-grid ">
+            <div className="company-card company-card--purple">
               <h3>{t('h3-assignment')}</h3>
               <p>{t('p-assignment')}</p>
             </div>
-            <div>
+            <div className="company-card company-card--blue">
               <h3>{t('h3-even-sponsorship')}</h3>
               <p>{t('p-even-sponsorship')}</p>
             </div>
-            <div>
+            <div className="company-card company-card--blue">
               <h3>{t('h3-guest-lectures')}</h3>
               <p>{t('p-guest-lectures')}</p>
             </div>
-            <div>
+            <div className="company-card company-card--purple">
               <h3>{t('h3-mentoring')}</h3>
               <p>{t('p-mentoring')}</p>
             </div>
@@ -75,37 +76,42 @@ const Companies = () => {
         </div>
       </section>
 
-      {/* Events */}
+      {/* events */}
       <section className="content__section">
         <div>
           <h2>{t('h2-events')}</h2>
         </div>
         <div className="simple-grid">
           <div>
-            <h3>{t('h3-events-kettuketer')}</h3>
-            <p>{t('p-events-kettuketer')}</p>
+            <ImageWrapper src={Img2} className="company2" />
           </div>
           <div>
-            <h3>{t('h3-events-kaunailta')}</h3>
-            <p>{t('p-events-kaunailta')}</p>
+            <div className="company-card company-card--purple">
+              <h3>{t('h3-events-kettuketer')}</h3>
+              <p>{t('p-events-kettuketer')}</p>
+            </div>
+            <div className="company-card company-card--purple">
+              <h3>{t('h3-events-kaunailta')}</h3>
+              <p>{t('p-events-kaunailta')}</p>
+            </div>
           </div>
         </div>
       </section>
 
-      {/* Experiences */}
-      <section className="content__section">
+      {/* experiences */}
+      <section className="content__section simple-grid">
         <div>
           <h2>{t('h2-experiences')}</h2>
         </div>
-        <div className="simple-grid">
-          <div>
+        <div className="simple-grid experience-card">
+          <div className="experience-card experience-card--alice">
             <p>{t('p-experience-1')}</p>
             <p>{t('p-experience-1-who')}</p>
             <p>{t('p-experience-1-company')}</p>
           </div>
-          <div>
+          <div className="experience-card experience-card--alice">
             <p>{t('p-experience-2')}</p>
-            <p>{t('p-experience-2-who')}</p>
+            <h5>{t('p-experience-2-who')}</h5>
             <p>{t('p-experience-2-company')}</p>
           </div>
         </div>
