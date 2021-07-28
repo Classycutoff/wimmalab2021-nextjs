723f88486b2469a745e33e201f370d72c1a50038
AA3737 <AA3737@student.jamk.fi>
2021-07-28 12:43:06 +0300

    Tried to get the nextchapter navigation to look better

--- components/NextChapter.js
+++ components/NextChapter.js
@@ -14,10 +14,10 @@ export default function NextChapter(props) {
 
   let pre_chap_pos, pre_chap_name, next_chap_pos, next_chap_name;
   if (chapterPos !== 0) {
-    [pre_chap_pos, pre_chap_name] = chapterList[chapterPos - 1].split('-');
+    [pre_chap_pos, ...pre_chap_name] = chapterList[chapterPos - 1].split('-');
   }
   if (chapterPos !== chapterList.length - 1) {
-    [next_chap_pos, next_chap_name] = chapterList[chapterPos + 1].split('-');
+    [next_chap_pos, ...next_chap_name] = chapterList[chapterPos + 1].split('-');
   }
 
   return chapterPos !== null ? (
@@ -25,15 +25,15 @@ export default function NextChapter(props) {
       <div className="switch-chapter">
         {chapterPos !== 0 && (
           <Link href={'../' + guideId + '/' + chapterList[chapterPos - 1]}>
-            <a>{pre_chap_pos + '. ' + pre_chap_name}</a>
+            <a className="ChapterBorder">{pre_chap_pos + '. ' + pre_chap_name.join(' ')}</a>
           </Link>
         )}
         <Link href={'../' + guideId}>
-          <a>Back To Guide Start</a>
+          <a className="ChapterBorder">Back To Guide Start</a>
         </Link>
         {chapterPos !== chapterList.length - 1 && (
           <Link href={'../' + guideId + '/' + chapterList[chapterPos + 1]}>
-            <a>{next_chap_pos + '. ' + next_chap_name}</a>
+            <a>{next_chap_pos + '. ' + next_chap_name.join(' ')}</a>
           </Link>
         )}
       </div>
